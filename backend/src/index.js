b1175655832b6f12d80b7e0b2430b71a5732d263
Padmaja <padmajachaure@gmail.com>
2026-03-29 21:24:41 +0530

    vercel link add

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,7 @@ initializeSocket(httpServer);
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://realtime-spotify-clone.vercel.app"],
+    origin: ["http://localhost:3000", "https://realtime-spotify-clone-n68apnbia-padmaja-chaures-projects.vercel.app"],
     credentials: true,
   })
 );
