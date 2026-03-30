270c7880b5ad6b8b057bb7b44b69ad873a7ac54f
Padmaja <padmajachaure@gmail.com>
2026-03-30 13:24:55 +0530

    fixed api baseURL

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5000/api"
-      : "https://realtime-spotify-clone-3-ehos.onrender.com/",
+      : "https://realtime-spotify-clone-3-ehos.onrender.com/api",
   withCredentials: true,
 });
