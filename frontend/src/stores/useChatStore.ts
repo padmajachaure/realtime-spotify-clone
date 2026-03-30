a45eb80560479d127249f4ebb81e39ed161c4d5f
Padmaja <padmajachaure@gmail.com>
2026-03-30 13:01:39 +0530

    fixed socket connection

--- frontend/src/stores/useChatStore.ts
+++ frontend/src/stores/useChatStore.ts
@@ -22,13 +22,17 @@ interface ChatStore {
 	setSelectedUser: (user: User | null) => void;
 }
 
-const baseURL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+const baseURL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000"
+    : "https://realtime-spotify-clone-3-ehos.onrender.com/";
 
-const socket = io(baseURL, {
-	autoConnect: false, // only connect if user is authenticated
-	withCredentials: true,
-});
-
+	
+		const socket = io(baseURL, {
+		autoConnect: false,
+		withCredentials: true,
+		transports: ["websocket"], // 🔥 important for Render
+		});
 export const useChatStore = create<ChatStore>((set, get) => ({
 	users: [],
 	isLoading: false,
