e098870b3ca80a69824983ed96a6098fae51700d
Padmaja <padmajachaure@gmail.com>
2026-04-02 12:14:03 +0530

    update CORS origin URL for consistency across backend files

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -6,7 +6,7 @@ export const initializeSocket = (server) => {
     cors: {
       origin: [
         "http://localhost:3000",
-        "https://realtime-spotify-clone-4svc3w6kx-padmaja-chaures-projects.vercel.app"
+        "https://realtime-spotify-clone-n8lz2fiz1-padmaja-chaures-projects.vercel.app"
       ],
       credentials: true,
     },
